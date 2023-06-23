a6811fb2fba64db17c26348cec6b4d22d383dec7
Mehdi Houasse <mehdi.houasse1@gmail.com>
2023-06-23 03:33:33 +0000

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,10 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+const firstName = languages[0];
+console.log(firstName);
+const thirdElement = languages[2];
+console.log(thirdElement);
+const length = languages.length;
+console.log(length);
+const lastElement = languages[languages.length - 1];
+console.log(lastElement);
